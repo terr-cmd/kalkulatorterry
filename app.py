2303d12432a6bde31e180e32b658bba161883354
terry <03081230030@student.uph.edu>
2026-03-09 20:22:06 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+print("APP RUNNING")    
+from calculator.calculator import add, subtract, multiply, divide
+
+print("Simple Calculator")
+
+a = 10
+b = 5
+
+print("Add:", add(a, b))
+print("Subtract:", subtract(a, b))
+print("Multiply:", multiply(a, b))
+print("Divide:", divide(a, b))
---
+++ tests/test_calculator.py
@@ -0,0 +1,7 @@
+from calculator.calculator import add
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_add_negative():
+    assert add(-1, -1) == -2
