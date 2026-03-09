fd95dc0a2dac193caaf4bc697ad269cb00f50879
terry <03081230030@student.uph.edu>
2026-03-09 21:02:37 +0700

    ubah test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,14 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from calculator.calculator import add
 
+
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == -1
+
 
 def test_add_negative():
     assert add(-1, -1) == -2
